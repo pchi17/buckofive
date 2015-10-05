f4785bcfba9544a111054eb630e7a39d74456392
Pengfei Philip Chi <pengfei.p.chi@gmail.com>
2015-10-05 03:37:44 -0700

    fix redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
 if Rails.env.production?
   require 'sidekiq'
-  
+
   Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDIS_PROVIDER'] }
+    config.redis = { url: ENV['REDISTOGO_URL'] }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { url: ENV['REDIS_PROVIDER'] }
+    config.redis = { url: ENV['REDISTOGO_URL'] }
   end
 end
