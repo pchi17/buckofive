a2cb94e8ab4959d02a38f0820f27f2252c61f15d
Pengfei Philip Chi <pengfei.p.chi@gmail.com>
2015-10-05 03:18:44 -0700

    fix REDISTOGO_URL

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 if Rails.env.production?
   Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDIS_PROVIDER'] }
+    config.redis = { url: URI.parse(ENV['REDIS_PROVIDER']) }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { url: ENV['REDIS_PROVIDER'] }
+    config.redis = { url: URI.parse(ENV['REDIS_PROVIDER']) }
   end
 end
